b93dfda1331c124b25569d86eb2b630d8ec6f7a9
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2019-10-19 16:28:23 -0700

    wip

--- game.py
+++ game.py
@@ -3,12 +3,14 @@
 
 import pdb
 
+from abc import ABC, abstractmethod
 import sys
-import logging
 import argparse
+from inspect import cleandoc
 import numpy as np
 
-# TODO implement real logging
+from utils import new_logger
+log = new_logger()
 
 # TODO: implement save and load for model
 def new_model():
@@ -28,12 +30,28 @@ def new_memory():
     memory = deque(maxlen=2000)
     return(memory)
 
-class Agent(object):
-    def __init__(self):
-        self.model = new_model()
-        self.memory = new_memory()
-        self.learningrate = 0.01
-        self.discount = 0.95
+class Agent(ABC):
+    # TODO actually maked this an abstract class
+    # TODO make the RandomAgent class
+
+    def __init__(self, value):
+        self.value = value
+        super().__init__()
+
+    @abstractmethod
+    def do_something(self):
+        pass
+
+    def __init__(self, tp):
+        validagenttypes = ['random', 'nnrl']
+        assert tp in validagenttypes, "Got agent type <{}>, must be in".format(
+            tp, validagenttypes)
+
+        if tp == 'nnrl':
+            self.model = new_model()
+            self.memory = new_memory()
+            self.learningrate = 0.01
+            self.discount = 0.95
 
     # model.fit(state, reward_value, epochs=1, verbose=0)
     # prediction = model.predict(state)
@@ -42,52 +60,52 @@ class Agent(object):
     # def remember(state, action, reward, next_state, done):
     #     memory.append((state, action, reward, next_state, done))
 
-
+class RandomAgent(Agent):
+    
 
 def new_board():
     board = np.matrix([[0,0,0], [0,0,0], [0,0,0]])
     return(board)
 
-def apply_move(x, y, wp, bd):
-    bd[x,y] = wp
-    return(bd)
+# def apply_move(x, y, wp, bd):
+#     bd[x,y] = wp
+#     return(bd)
 
-def is_legal(x, y, bd):
-    r = True
-    if x > 2 or x < 0: r = False
-    if y > 2 or y < 0: r = False
-    if bd[x,y] != 0: r = False
-    return(r)
+# def is_legal(x, y, bd):
+#     if x > 2 or x < 0: return(False)
+#     if y > 2 or y < 0: return(False)
+#     if bd[x,y] != 0: return(False)
+#     return(True)
 
-def won(bd):
-    r = False
-    for x in range(3):
-        # check rows, then columns
-        if (bd[x,:] == 1).all() or (bd[x,:] == 2).all(): r = True
-        if (bd[:,x] == 1).all() or (bd[:,x] == 2).all(): r = True
-    # diags
-    if (bd.diagonal() == 1).all() or (bd.diagonal() == 2).all(): r = True
-    b2 = np.rot90(bd)
-    if (b2.diagonal() == 1).all() or (b2.diagonal() == 2).all(): r = True
-    return(r)
+# def won(bd):
+#     r = False
+#     for x in range(3):
+#         # check rows, then columns
+#         if (bd[x,:] == 1).all() or (bd[x,:] == 2).all(): r = True
+#         if (bd[:,x] == 1).all() or (bd[:,x] == 2).all(): r = True
+#     # diags
+#     if (bd.diagonal() == 1).all() or (bd.diagonal() == 2).all(): r = True
+#     b2 = np.rot90(bd)
+#     if (b2.diagonal() == 1).all() or (b2.diagonal() == 2).all(): r = True
+#     return(r)
 
 class Board(object):
+
     def __init__(self):
         self.state = new_board()
         self.gameover = False
         self.whichplayer = 1
 
     def __repr__(self):
-        print("<BOARD>")
-        print("It's player {}'s turn.".format(self.whichplayer))
-        print(self.state)
+        msg = """<BOARD>\nIt's player {}'s turn.\n{}""".format(
+            self.whichplayer, self.state)
+        return(cleandoc(msg))
 
     def _is_legal(self, x, y):
-        r = True
-        if x > 2 or x < 0: r = False
-        if y > 2 or y < 0: r = False
-        if self.state[x,y] != 0: r = False
-        return(r)
+        if x > 2 or x < 0: return(False)
+        if y > 2 or y < 0: return(False)
+        if self.state[x,y] != 0: return(False)
+        return(True)
 
     def _apply_move(self, x, y, wp):
         self.state[x,y] = wp
@@ -95,27 +113,57 @@ class Board(object):
     def _next_player(self):
         self.whichplayer = (self.whichplayer % 2) + 1
 
-    def move(self, x, y, wp):
-        """If move is legal, apply it. Return reward."""
+    def _won(self):
+        """Using current state, return whether anyone has won"""
+        # TODO: program ties
+        bd = self.state
+        for x in range(3):
+            # check rows, then columns
+            if (bd[x,:] == 1).all() or (bd[x,:] == 2).all(): return(True)
+            if (bd[:,x] == 1).all() or (bd[:,x] == 2).all(): return(True)
+        # diags
+        if (bd.diagonal() == 1).all() or (bd.diagonal() == 2).all(): return(True)
+        b2 = np.rot90(bd)
+        if (b2.diagonal() == 1).all() or (b2.diagonal() == 2).all(): return(True)
+        return(False)
+
+    def move(self, x, y):
+        """If move is legal, apply it and switch players. Return reward."""
         if self._is_legal(x, y):
+            log.debug("Legal move {} {}, applying to board for player {}".format(
+                x, y, self.whichplayer))
+            self._apply_move(x, y, self.whichplayer)
+            log.debug("Applied moved to board")
+            switchplayer = True
             r = 0.1 # small reward for legal move
-            self._apply_move(x, y, wp)
-            self._next_player()
         else:
+            log.debug("Illegal move {} {}".format(x, y))
+            switchplayer = False
             r = -0.1 # negative reward for illegal move
-        if won # TODO: continue here
+        if self._won():
+            log.debug("Player {} won".format(self.whichplayer))
+            self.gameover = True
+            switchplayer = False
+            r = 1 # big reward for winning
+        if switchplayer:
+            log.debug("Switching player, currently {}".format(self.whichplayer))
+            self._next_player()
+            log.debug("Player is now {}".format(self.whichplayer))
+        return(r)
 
 def train():
     """Play a fixed number of games
     In each game, agent moves and gets rewards.
     """
     epoch = 10
-    agents = (Agent(), Agent())
+    agents = (Agent('random'), Agent('random'))
     for ng in range(epoch):
-        print("Starting game {}".format(ng))
+        log.info("Starting game {}".format(ng))
         bd = Board()
-        print(bd)
+        log.info(bd)
         while not bd.gameover:
+            # TODO: make random policy agent
+            # TODO: make nnrl agent
             agent = agents[bd.whichplayer-1]
             pm = agent.propose_move()
             r = bd.move(pm)
@@ -123,23 +171,14 @@ def train():
 
 def game():
     """Interactive gameloop for playing 2 humans in cmdline"""
-    # TODO: use Board
-    gameover = False
-    board = new_board()
-    whichplayer = 1
-    while not gameover:
-        print(board)
-        i = input("Player {}, please enter your move: ".format(whichplayer))
-        mx = int(i[0])
-        my = int(i[1])
-        if is_legal(mx, my, board):
-            board = apply_move(mx, my, whichplayer, board)
-            if won(board): gameover = True
-            else: whichplayer = (whichplayer % 2) + 1
-        else:
-            print("Illegal move!")
-    print("Congrats player {}, you won!".format(whichplayer))
-    return(whichplayer) # return who won
+    board = Board()
+    while not board.gameover: # get move from player, apply game logic
+        log.info(board)
+        i = input("Player {}, please enter your move 'yx': ".format( board.whichplayer))
+        mx, my = tuple(map(lambda x: int(x), i))
+        log.debug("Got move {} {}".format(mx,my))
+        _ = board.move(mx, my) # discard reward
+    log.info("Congrats player {}, you won!".format(board.whichplayer))
 
 def main():
     parser = argparse.ArgumentParser(description='TicTacToe game')
@@ -149,7 +188,9 @@ def main():
     if not args.train: game()
     else: train()
 
-def quit(): sys.exit(0)
+def quit():
+    log.info("\nQuitting...")
+    sys.exit(0)
 
 if __name__ == "__main__":
     try: main()
